bf2f9dc332fd7290f3206da9577b165a2038ac40
Victoria <baby_94_777@mail.ru>
2022-06-08 20:42:44 +0300

    commit examp5 proverka

---
+++ Desktop/Разработчик/Знакомство с языками/lec_1_new/examp/examp5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Vvedite name:  ");
+string username = Console.ReadLine();
+
+if (username == "Masha")
+{
+    Console.WriteLine("oooo, Masha")
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
